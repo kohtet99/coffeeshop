2084ad510e93161870e7a900138d33a4139ed676
kohtet99 <kokohtet100000@gmail.com>
2024-12-31 16:04:03 +0630

    com

--- config/db.config.js
+++ config/db.config.js
@@ -4,15 +4,12 @@ module.exports = async () => {
   try {
     await mongoose
       .connect(process.env.MONGODB_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
+        
       })
       .then((conn) =>
         console.log(`DB is connected with ${conn.connection.host}`)
       )
-      .catch((err) => {
-        console.log(err);
-      });
+      
   } catch (err) {
     console.log(err.message);
   }
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
 const dbConnect = require("./config/db.config.js");
-const controller = require("./controllers/customer.controller.js");
+
 const router = require("./routers/router.js");
 
 dotenv.config();
-dbConnect();
+
 
 const app = express();
 
@@ -20,10 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/cms", router);
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+})
+
 app.use("/uploads", express.static("uploads"));
 
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
+  dbConnect();
 });
